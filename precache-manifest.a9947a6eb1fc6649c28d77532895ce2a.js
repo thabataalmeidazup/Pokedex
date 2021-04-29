ce547eed1d0b8cf9d3b4fbb4e7baa9b45e497e9e
Thabata <thabata.almeida@zup.com.br>
2021-04-29 11:11:20 -0300

    Updates

---
+++ precache-manifest.a9947a6eb1fc6649c28d77532895ce2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9cf05ed192e80c376fe",
+    "url": "/static/css/main.5784d54e.chunk.css"
+  },
+  {
+    "revision": "b9cf05ed192e80c376fe",
+    "url": "/static/js/main.b9cf05ed.chunk.js"
+  },
+  {
+    "revision": "44c0fbc9109c17ab8bd1",
+    "url": "/static/js/1.44c0fbc9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ecf5f5408171f868e11af8a42b392b6",
+    "url": "/index.html"
+  }
+];
